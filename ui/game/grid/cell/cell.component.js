1db1a216e1d56adccc743f16ecc4c9985d2d2028
MaximOEgorov <llkk67@gmail.com>
2024-03-01 22:51:40 +0300

    Commit #12 -- grid render

--- ui/game/grid/cell/cell.component.js
+++ ui/game/grid/cell/cell.component.js
@@ -1,58 +1,21 @@
 import {Google, Player1, Player2} from "./google/google.component.js";
-import {
-    subscribe, unsubscribe,
-    getGoogleCoordinates, getPlayer1Position,
-    getPlayer2Position} from "../../../../data.adapter.js";
-import {EVENTS} from "../../../../back/EVENTS.js";
 
-export async function Cell(x, y) {
+export function Cell(x, y, imgRole) {
     const cellElement = document.createElement("td");
-    let prevState = 'empty';
 
-    async function getCurrentState() {
-        const googlePosition = await getGoogleCoordinates();
-        const player1Position = await getPlayer1Position();
-        const player2Position = await getPlayer2Position();
-        if (x === googlePosition.x && y === googlePosition.y) {
-            return 'google';
-        } else if (x === player1Position.x && y === player1Position.y) {
-            return 'player1';
-        } else if (x === player2Position.x && y === player2Position.y) {
-            return 'player2';
-        } else return 'empty';
+    switch (imgRole) {
+        case 'google':
+            cellElement.append(Google());
+            break;
+        case 'player1':
+            cellElement.append(Player1());
+            break;
+        case 'player2':
+            cellElement.append(Player2());
+            break;
+        case 'empty':
+            break;
     }
-
-    async function render() {
-        cellElement.innerHTML = '';
-        const currentState = await getCurrentState();
-
-        unsubscribe(EVENTS.GOOGLE_JUMPED, render);
-        unsubscribe(EVENTS.PLAYER1_MOVED, render);
-        unsubscribe(EVENTS.PLAYER2_MOVED, render);
-
-        switch (currentState) {
-            case 'google':
-                subscribe(EVENTS.GOOGLE_JUMPED, render);
-                cellElement.append(Google());
-                break;
-            case 'player1':
-                subscribe(EVENTS.PLAYER1_MOVED, render);
-                cellElement.append(Player1());
-                break;
-            case 'player2':
-                subscribe(EVENTS.PLAYER2_MOVED, render);
-                cellElement.append(Player2());
-                break;
-            case 'empty':
-                subscribe(EVENTS.GOOGLE_JUMPED, render);
-                subscribe(EVENTS.PLAYER1_MOVED, render);
-                subscribe(EVENTS.PLAYER2_MOVED, render);
-                break;
-        }
-        prevState = currentState;
-    }
-
-    await render();
 
     return cellElement;
 }
--- ui/game/grid/grid.component.js
+++ ui/game/grid/grid.component.js
@@ -1,31 +1,61 @@
 import {
-  getGridSize,
-  subscribe,
+    getGoogleCoordinates,
+    getGridSize, getPlayer1Position, getPlayer2Position,
+    subscribe,
 } from "../../../data.adapter.js";
-import { Cell } from "./cell/cell.component.js";
+import {Cell} from "./cell/cell.component.js";
+import {EVENTS} from "../../../back/EVENTS.js";
+
+async function getCurrentState() {
+    const googlePosition = await getGoogleCoordinates();
+    const player1Position = await getPlayer1Position();
+    const player2Position = await getPlayer2Position();
+    return {
+        googlePosition: googlePosition,
+        player1Position: player1Position,
+        player2Position: player2Position,
+    }
+}
 
 export async function GameGrid() {
-  const containerElement = document.createElement("table");
+    const containerElement = document.createElement("table");
+    subscribe(EVENTS.GOOGLE_JUMPED, () => {
+        render(containerElement)
+    });
+    subscribe(EVENTS.PLAYER1_MOVED, () => {
+        render(containerElement)
+    });
+    subscribe(EVENTS.PLAYER2_MOVED, () => {
+        render(containerElement)
+    });
 
-/*  subscribe(() => {
-    update(containerElement);
-  });*/
-  await update(containerElement);
 
-  return containerElement;
-}
-async function update(containerEl) {
-  const gridSize = await getGridSize();
+    async function render(containerEl) {
+        const gridSize = await getGridSize();
+        const CurrentState = await getCurrentState();
 
-  containerEl.innerHTML = "";
-  for (let y = 0; y < gridSize.rowsCount; y++) {
-    const row = document.createElement("tr");
+        containerEl.innerHTML = "";
+        for (let y = 0; y < gridSize.rowsCount; y++) {
+            const row = document.createElement("tr");
 
-    for (let x = 0; x < gridSize.columnsCount; x++) {
-      const cell = await Cell(x, y);
+            for (let x = 0; x < gridSize.columnsCount; x++) {
+                let imgRole = 'empty';
+                if (x === CurrentState.googlePosition.x && y === CurrentState.googlePosition.y) {
+                    imgRole = 'google';
+                } else if (x === CurrentState.player1Position.x && y === CurrentState.player1Position.y) {
+                    imgRole = 'player1';
+                } else if (x === CurrentState.player2Position.x && y === CurrentState.player2Position.y) {
+                    imgRole = 'player2';
+                }
+                const cell = Cell(x, y, imgRole);
 
-      row.append(cell);
+                row.append(cell);
+            }
+            containerEl.append(row);
+        }
     }
-    containerEl.append(row);
-  }
+
+    render(containerElement);
+    return containerElement;
+
 }
